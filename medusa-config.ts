533c9cc488f4b0fcd14dfa0f0baf89eb154363fc
Meenakshi <ymeenakpm@gmail.com>
2025-11-22 14:52:20 -0600

    Add redis_Url to Medusa projectConfig

--- medusa-config.ts
+++ medusa-config.ts
@@ -4,12 +4,12 @@ loadEnv(process.env.NODE_ENV || 'development', process.cwd())
 
 module.exports = defineConfig({
   projectConfig: {
-    databaseUrl: process.env.DATABASE_URL,
-    redisUrl: process.env.REDIS_URL,
+    database_url: process.env.DATABASE_URL,
+    redis_url: process.env.REDIS_URL,
     http: {
-      storeCors: process.env.STORE_CORS!,
-      adminCors: process.env.ADMIN_CORS!,
-      authCors: process.env.AUTH_CORS!,
+      storeCors: process.env.STORE_CORS,
+      adminCors: process.env.ADMIN_CORS,
+      authCors: process.env.AUTH_CORS,
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",
     }
